ec1c3f74e80d790166c66c6bb66c0b1150bca065
Bernar <67651190+bernar-zholdybek@users.noreply.github.com>
2026-06-11 09:03:08 +0800

    Update devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,8 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/basics/
   env.GREET = "devenv";
 
-  # https://devenv.sh/packages/
   packages = [
     pkgs.git
     pkgs.jupyter
@@ -16,41 +14,5 @@
     pkgs.python313Packages.pandas
     pkgs.python313Packages.seaborn
   ];
-
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  # https://devenv.sh/basics/
-  enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
+  
 }
